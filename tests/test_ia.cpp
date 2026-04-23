096c63ff763ed52866f3a45643c927bd4efa3801
Gael Amoros <gaelamoros13@gmail.com>
2026-04-23 14:37:52 +0200

    test(ia): couverture clone() flag false + suppression include inutile

--- tests/test_ia.cpp
+++ tests/test_ia.cpp
@@ -1,6 +1,5 @@
 // tests/test_ia.cpp
 #include <gtest/gtest.h>
-#include "modele/Partie.h"
 #include "modele/PieceFactory.h"
 
 using namespace yalta;
@@ -15,5 +14,14 @@ TEST(PieceClone, ClonerConserveAttributs) {
     EXPECT_EQ(copie->getCouleur(),  Couleur::BLANC);
     EXPECT_EQ(copie->getPosition(), (Position{0, 'e', 1}));
     EXPECT_TRUE(copie->aDejaBouge());
-    EXPECT_NE(copie.get(), piece.get());  // objet distinct
+    EXPECT_NE(copie.get(), piece.get());
+}
+
+TEST(PieceClone, ClonePieceNonDeplaceeGardeFlagFalse) {
+    auto piece = PieceFactory::creer(TypePiece::PION, Couleur::NOIR,
+                                     Position{1, 'a', 2});
+    ASSERT_FALSE(piece->aDejaBouge());
+    auto copie = piece->clone();
+    EXPECT_FALSE(copie->aDejaBouge());
+    EXPECT_NE(copie.get(), piece.get());
 }
